da6e2503c7d3b4cf92407c6ef4cbcd6959bcceea
ins1gn1a <ins1gn1a@users.noreply.github.com>
2015-12-31 12:09:27 +0000

    Added additional functionality
    Username search
    Exact string search

--- pwdlyser.py
+++ pwdlyser.py
@@ -4,6 +4,7 @@
 To Do:
 
 * Identify domain admins/admins/etc from an imported list (take admin list, highlight any of those).
+* Identify multiple shared passwords.
 * Fix -oR reporting stdout output.
 * Dollar dollar bill y'all
 '''
@@ -13,6 +14,7 @@ import argparse
 from string import digits
 import re
 from collections import Counter
+from collections import defaultdict
 
 parser = argparse.ArgumentParser(description='Password Analyser')
 parser.add_argument('-p','--pass-list',dest='pass_list',help='Enter the path to the list of passwords, either in the format of passwords, or username:password.',required=True)
@@ -24,6 +26,9 @@ parser.add_argument('-s','--search',dest='basic_search',help='Run a basic search
 parser.add_argument('-oR',dest='output_report',help='Output format set for reporting with "- " prefix',action='store_true',default=False)
 parser.add_argument('-c','--common',dest='common_pass',help='Check against list of common passwords',action='store_true',default=False)
 parser.add_argument('-f','--freq',dest='freq_anal',help='Perform frequency analysis',required=False,type=int)
+parser.add_argument('--exact',dest='exact_search',help='Perform a search using the exact string.')
+parser.add_argument('-u','--user',dest='user_search',help='Return usernames that match string (case insensitive)')
+#parser.add_argument('--shared',dest='shared_pass',help='Display any reused/shared passwords.',required=False,action='store_true',default=False)
 args = parser.parse_args()
 
 pass_list = args.pass_list
@@ -47,14 +52,22 @@ def output_pass(username,password,issue):
 
     else:
         # Username:Pass
-        print (username.ljust(30),end=":".ljust(5),flush=True)
-        print (password.ljust(30),end=":".ljust(5),flush=True)
+        print (username.ljust(50),end=":".ljust(5),flush=True)
+        print (password.ljust(35),end=":".ljust(5),flush=True)
         print (issue)
 
 # Check for inputted min length
 def check_min_length(password,min):
     if (len(password) < min) or (password == "*******BLANK-PASS*******"):
         output_pass(user,pwd,"Length < " + str(args.min_length))
+
+def check_user_search(user,password,term):
+    if term.lower() in user.lower():
+        output_pass(user,password,"Username Search: " + term)
+
+def check_exact_search(user,password,term):
+    if term in password:
+        output_pass(user,password,"Term " + term + " in password")
 
 # Check for org name (reused code from below, laziness)
 def check_org_name(user,password,org):
@@ -163,55 +176,110 @@ def check_frequency_analysis(full_list,length):
             output_pass(pair[0],str(pair[1]),"")
             z += 1
 
+''' 
+#def check_shared_pass(full_list):
+#    z = 0
+#    pwd_list = []
+#    words = Counter()
+
+    for pwd in full_list:
+        x = pwd[1]
+        if x == "":
+            x = "*******BLANK-PASS*******"
+        pwd_list.append(x)
+
+    words.update(pwd_list)
+    wordfreq = (words.most_common())
+
+    comp_list = []
+    for pair in wordfreq:
+        if z < 10:
+            comp_list.append(pair)
+            z += 1
+    for dup in sorted(list_duplicates(full_list)):
+        print (dup)
+
+
+def list_duplicates(seq):
+    tally = defaultdict(list)
+    for i,item in enumerate(seq):
+        tally[item].append(i)
+    return ((key,locs) for key,locs in tally.items() 
+                            if len(locs)>1)
+
+
+#    print (comp_list)
+
+#    seen = set(full_list)
+#    uniq = []
+#    for q in comp_list:
+#        if q[1] not in seen:
+#            uniq.append(q)
+#            seen.add(q)
+
+
+#    for l in uniq:
+#        output_pass(l[0],str(l[1]),"")
+   
+'''
+ 
 # Run main stuff
 if __name__ == "__main__":
 
-     # Retrieve list
-     full_list = (delimit_list(pass_list))
-     y = 0
+    # Retrieve list
+    full_list = (delimit_list(pass_list))
+    y = 0
 
-     if args.freq_anal is None:
+    if args.freq_anal is None:
      
-         # Headers
-         output_pass("-" * 30,"-" * 30,"-" * 30)
-         output_pass("Username","Password","Description")
-         output_pass("-" * 30,"-" * 30,"-" * 30)
-     else:
-         output_pass("-" * 30,"-" * 30,"")
-         output_pass("Password","Frequency","")
-         output_pass("-" * 30,"-" * 30,"")
+        # Headers
+        output_pass("-" * 30,"-" * 30,"-" * 30)
+        output_pass("Username","Password","Description")
+        output_pass("-" * 30,"-" * 30,"-" * 30)
  
-     # Cycle through output list
-     for item in full_list:
-         # removed above code as pointless to use only passwords
-         user = item[0]
-         pwd = item[1]
+    # Cycle through output list
+    for item in full_list:
+        # removed above code as pointless to use only passwords
+        user = item[0]
+        pwd = item[1]
+        if pwd == "":
+            pwd = "*******BLANK-PASS*******"
 
-         if pwd == "":
-             pwd = "*******BLANK-PASS*******"
+        # Print everything regardless
+        if args.print_all:
+            output_pass(user,pwd,"Not Analysed")
+            continue # Skip analysis functions below
 
-         # Print everything regardless
-         if args.print_all:
-             output_pass(user,pwd,"Not Analysed")
-             continue # Skip analysis functions below
+        # Check Min Length
+        if (args.min_length is not None):
+            issue_old = None
+            check_min_length(pwd,args.min_length)
 
-         # Check Min Length
-         if (args.min_length is not None):
-             issue_old = None
-             check_min_length(pwd,args.min_length)
+        # Check if Org name (or slight variation) is in list
+        if organisation is not None:
+            issue_old = None
+            check_org_name(user,pwd,organisation)
 
-         # Check if Org name (or slight variation) is in list
-         if organisation is not None:
-             issue_old = None
-             check_org_name(user,pwd,organisation)
+        # Basic search             
+        if args.basic_search is not None:
+            check_basic_search(user,pwd)
 
-         # Basic search             
-         if args.basic_search is not None:
-             check_basic_search(user,pwd)
+        # Common Passwords
+        if args.common_pass is True:
+            check_common_pass(user,pwd)
 
-         # Common Passwords
-         if args.common_pass is True:
-             check_common_pass(user,pwd)
+        if args.exact_search is not None:
+            check_exact_search(user,pwd,args.exact_search)
+
+        if args.user_search is not None:
+            check_user_search(user,pwd,args.user_search)
+
+#    if args.shared_pass:
+#        check_shared_pass(full_list)
  
-     if args.freq_anal is not None:
-         check_frequency_analysis(full_list,args.freq_anal)
+    if args.freq_anal is not None:
+        output_pass("-" * 30,"-" * 30,"")
+        output_pass("Password","Frequency","")
+        output_pass("-" * 30,"-" * 30,"")
+        
+        check_frequency_analysis(full_list,args.freq_anal)
