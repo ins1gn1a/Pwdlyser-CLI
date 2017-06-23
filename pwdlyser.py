fc7c54b2b30ddb68baf0957e1695bf74abf65853
Adam <ins1gn1a@users.noreply.github.com>
2017-06-23 10:39:22 +0100

    Added password reuse between singular accounts
    -r --reuse options
    v2.5.0

--- pwdlyser.py
+++ pwdlyser.py
@@ -3,7 +3,7 @@
 
 __author__ = "Adam Govier"
 __license__ = "MIT"
-__version__ = "2.4.8"
+__version__ = "2.5.0"
 __maintainer__ = "ins1gn1a"
 __status__ = "Production"
 
@@ -40,6 +40,7 @@ parser.add_argument('-l','--length',dest='min_length',help='Display passwords th
 parser.add_argument('-m','--mask',dest='masks',help='Display the most commonly used Hashcat masks. This is extremely useful for further cracking attacks',action='store_true',required=False,default=False)
 parser.add_argument('-mc','--mask-count',dest='masks_results_count',help='(Optional) Specify the number of mask to output for the -m / --masks option',default=25,required=False,type=int)
 parser.add_argument('-o','--org-name',dest='org_name',help='Enter the organisation name or abbreviation to identify any users that have a variation in their password. Usage exmaple: "-o google"',required=False)
+parser.add_argument('-r','--reuse',dest='reuse_pass',help='List user accounts and masked passwords that re-use passwords between low-privileged and high-privileged accounts.',required=False,action='store_true',default=False)
 parser.add_argument('-S','--search',dest='basic_search',help='Run a basic search using a keyword. Non-alpha characters will be stripped, i.e. syst3m will become systm (although this will be compared against the same stripped passwords',required=False)
 parser.add_argument('-s','--shared',dest='shared_pass',help='Display any passwords that appear more than once in the list. This is useful for identifying accounts that reuse passwords, such as service accounts or administrators',required=False,action='store_true',default=False)
 parser.add_argument('-u','--user',dest='user_search',help='Search for usernames that contain all or part of the input string (case insensitive). Usage example: "-u ins1g"',required=False)
@@ -168,7 +169,7 @@ def check_user_as_pass(user,pwd):
         tmp = check_basic_search(user,pwd,user)
 
         if tmp:
-            return (1)    
+            return (1)
     else:
         check_basic_search(user,pwd,user)
 
@@ -177,7 +178,7 @@ def check_basic_search(user,password,search):
     x = 0
     pwd_unleet = password
     leet_list = reverse_leet_speak()
-    
+
     for line in leet_list:
         if "," in line:
             char_change = line.split(",")
@@ -283,7 +284,7 @@ def delimit_list(list):
                 except:
                     n = 0
                     check_hash_delimit = False
-            
+
             # Delimits with hash username:hash:password or username:password
             if n != 0:
                 try: # Try to delimit user:hash:password
@@ -292,7 +293,7 @@ def delimit_list(list):
                     try:
                         list_stuff = [list_entry.split(":",2)[0],list_entry.split(":",2)[1]]
                     except: # Everything has gone wrong
-                        print ("[!] Can't split input line: " + str(file_line_count)) 
+                        print ("[!] Can't split input line: " + str(file_line_count))
             else:
                 list_stuff = list_entry.split(":",1)
             if (len(list_stuff)) == 1: # Can't remember what this does
@@ -364,28 +365,28 @@ def password_masking(x):
     if x == "*******BLANK-PASS*******":
         mask_pwd = x
     else:
-        # Output Masking                
+        # Output Masking
         if len(x) >= 9:
             mask_pwd = x[0:3] + ((len(x) - 6) * "*") + x[-3:]
 
         elif len(x) >= 5 and len(x) <= 8:
             mask_pwd = x[0:2] + ((len(x) - 4) * "*") + x[-2:]
         elif len(x) == 4:
-            mask_pwd = x[0:1] + ((len(x) - 2) * "*") + x[-1:]       
+            mask_pwd = x[0:1] + ((len(x) - 2) * "*") + x[-1:]
         elif len(x) == 3:
-            mask_pwd = x[0:1] + ((len(x) - 2) * "*") + x[-1:]                     
+            mask_pwd = x[0:1] + ((len(x) - 2) * "*") + x[-1:]
         elif len(x) == 2:
-            mask_pwd = x[0:1] + ((len(x) - 1) * "*") 
+            mask_pwd = x[0:1] + ((len(x) - 1) * "*")
         else:
             mask_pwd = x
-        
+
     return mask_pwd
 
 def check_shared_pass(full_list):
     a = ([])
     for item in full_list:
         a.append(item[1])
-        
+
     # Sort as collection
     y=collections.Counter(a)
     pwd_list = [i for i in y if y[i]>1]
@@ -395,13 +396,34 @@ def check_shared_pass(full_list):
         for z in pwd_list:
             if x[1] == z:
                 #if ((len(x[1]) % 2) > 0):
-                
+
                 if args.output_report:
                     print_report(str(x[0]) + " : " + password_masking(x[1]))
                 else:
                     output_pass(x[0],str(x[1]),"Password Re-Use")
 
-    
+def check_reuse_pass(full_list,z):
+    a = ([])
+    for item in full_list:
+        a.append(item[1])
+
+    # Sort as collection
+    #y=collections.Counter(a)
+    #pwd_list = [i for i in y if y[i]>1]
+
+    # Identifying duplicates and outputting
+    for x in full_list:
+        for user in full_list:
+            if (user[0] != x[0]) and (user[0].lower() in x[0] and user[1] == x[1]):
+                if args.output_report or z == 'summary':
+                    print_report(str(x[0]) + " : " + password_masking(x[1]))
+                    print_report(str(user[0]) + " : " + password_masking(user[1]))
+                else:
+                    output_pass(x[0],str(x[1]),"Password Re-Use: " + x[0])
+                    output_pass(user[0],str(user[1]),"Password Re-Use: " + user[0])
+
+# reuse_pass
+
 # Run character analysis
 
 # Perform analysis analysis
@@ -481,10 +503,10 @@ def hashcat_mask_analysis(full_list):
     words = Counter()
     mask_list = []
 
-    for x in full_list:    
-        password = x[1]  
-        full_mask = "" 
-        mask = []     
+    for x in full_list:
+        password = x[1]
+        full_mask = ""
+        mask = []
 
         # Loop through each character in password string and regex against mask type
         for char in password:
@@ -498,23 +520,23 @@ def hashcat_mask_analysis(full_list):
                 mask.append("?s")
             else:
                 pass
-                
+
         for z in mask:
             full_mask = full_mask + z
-            
+
         if (len(full_mask) / 2) == len(password):
             mask_list.append(full_mask)
-                                
+
     words.update(mask_list)
     wordfreq = (words.most_common())
-    
+
     w = 0
     ast = "*"
     mostUsed = (wordfreq[0])[1]
-        
+
     if args.output_report:
         print ("The top 10 Hashcat masks:")
-        
+
 
     for m in wordfreq:
         if w != args.masks_results_count: # Limit output to 10 most common entries
@@ -526,8 +548,8 @@ def hashcat_mask_analysis(full_list):
                 output_pass(str(m[0]),mask_length,mask_occurrence)
                 w += 1
     print ("")
-        
-    
+
+
 def keyboard_patterns(full_list):
     keyboard_list = ["hjkl","asdf","lkjh","qwerty","qwer","zaqwsx","zaqxsw","qazwsx","qazxsw","zxc","zxcvbn","zxcdsa","1qaz","2wsx","poiuy","mnbvc","plm","nkoplm","qwer1234","2468","1357","3579","0864"]
     total_count = 0
@@ -560,7 +582,7 @@ def remove_end_numeric(pass_list):
         z = False
 
         break_check_length = len(p)
-        
+
         if (len(p) == 0):
             continue
 
@@ -580,7 +602,7 @@ def remove_end_numeric(pass_list):
     for pwd in cleaned_pass_list:
         f.write(pwd + '\n')
     f.close()
-    
+
 def entropy_calculate(full_list):
     words = Counter()
     entropy_list = []
@@ -610,11 +632,11 @@ def entropy_calculate(full_list):
 #             else: # 33 Special chars as per Hashcat !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ (including space)
 #                 char_space += 33
         #entropy = L * math.log2(char_space)
-        
+
         for x in e:
             count += x
         entropy_list.append([int(count),pwd])
-   
+
     sorted_ent_list = sorted(entropy_list, reverse=True)
     if args.output_report:
         print ("\nThe following items are the top 10 estimated 'strongest' passwords (by entropy) that were able to be computed: ")
@@ -622,7 +644,7 @@ def entropy_calculate(full_list):
         output_pass("-" * 30,"-" * 30,"")
         output_pass("Entropy","Password","")
         output_pass("-" * 30,"-" * 30,"")
-        
+
     w = 0 # Max output for top passwords
     for m in sorted_ent_list:
 
@@ -643,7 +665,7 @@ def entropy_calculate(full_list):
         output_pass("-" * 30,"-" * 30,"")
         output_pass("Entropy","Password","")
         output_pass("-" * 30,"-" * 30,"")
-        
+
     w = 0 # Max output for top passwords
     for m in sorted_ent_list:
 
@@ -678,6 +700,7 @@ if __name__ == "__main__":
     date_day_count = 0
     shared_count = 0
     keyboard_count = 0
+    reuse_count = 0
 
     if (args.output_report is False):
         if ((args.freq_anal is None) and (args.freq_len is None) and (args.masks is None)):
@@ -703,6 +726,16 @@ if __name__ == "__main__":
 
             check_frequency_analysis(full_list,args.freq_anal)
 
+    if args.reuse_pass:
+        if args.output_report:
+            print ("\nThe following accounts were found to share the same password between similarly named accounts. These are believed to be service accounts or individual user accounts that are operated by the same user. Password re-use should be investigated as when a password is reused between a low privileged and a high privileged account it can lead to a compromise of systems that the high privileged account is authorised to access:")
+            check_reuse_pass(full_list,'summary')
+        else:
+            output_pass("-" * 30,"-" * 30,"")
+            output_pass("Username","Password","Description")
+            output_pass("-" * 30,"-" * 30,"")
+            check_reuse_pass(full_list,'')
+
     elif args.freq_len is not None:
         if args.output_report:
             print ("The following is a descending list of the most popular password lengths: ")
@@ -712,15 +745,15 @@ if __name__ == "__main__":
             output_pass("Password Length","Frequency","")
             output_pass("-" * 30,"-" * 30,"")
             check_frequency_length(full_list,args.freq_len)
-            
-    
+
+
 
     elif args.char_anal:
         check_character_analysis(full_list)
-        
+
     elif args.summary:
         print ("A password audit was performed against the extracted password hashes. Password cracking methods and tools were used to enumerate the plaintext password counterparts, and as such not all of the passwords were able to be identified. In total, there were " + str(len(full_list)) + " username and password combinations that were obtained and have been analysed.")
-        
+
         # Top 10 most used passwords
         print ("\nAs part of the password audit, the top 10 most commonly used passwords within the organisation have been compiled. This list has been broken up with the password, the percentage of the total passwords, and the numeric value of the total passwords:")
         check_frequency_analysis(full_list,10)
@@ -756,13 +789,13 @@ if __name__ == "__main__":
             pwd = item[1]
             if pwd == "":
                 pwd = "*******BLANK-PASS*******"
-            
+
             if (check_user_as_pass(user,pwd)) == 1:
                 userpass_summary_count += 1
         if (userpass_summary_count > 0):
-            print ("\nThere were " + str(userpass_summary_count) + " passwords that were identified as having a password set that was a variation of the username; this includes additional prefixed or suffixed characters, substitutions within the word (i.e. 3 instead of e), or the username as it appears. Penetration testers, and more importantly attackers, will often check system or administrative accounts that have a variation of the username set as the password and as such it is critical that organisations do not use this convention for password security." )            
+            print ("\nThere were " + str(userpass_summary_count) + " passwords that were identified as having a password set that was a variation of the username; this includes additional prefixed or suffixed characters, substitutions within the word (i.e. 3 instead of e), or the username as it appears. Penetration testers, and more importantly attackers, will often check system or administrative accounts that have a variation of the username set as the password and as such it is critical that organisations do not use this convention for password security." )
 
-        # Count of organisation name in password 
+        # Count of organisation name in password
         # Requires -o parameter
         if args.org_name:
             org_summary_count = 0
@@ -776,6 +809,11 @@ if __name__ == "__main__":
             if (org_summary_count) > 0:
                 print ("\nThe organisation name, or a variation of the name (such as an abbreviation), " + args.org_name + " was found to appear within " + str(org_summary_count) +  " of the passwords that were able to be obtained during the password audit. For any system or administrative user accounts that have a variation of the company name as their password, it is highly recommended that the passwords are changed to prevent targeted guessing attacks.")
 
+
+        print ("\nThe following accounts were found to share the same password between similarly named accounts. These are believed to be service accounts or individual user accounts that are operated by the same user. Password re-use should be investigated as when a password is reused between a low privileged and a high privileged account it can lead to a compromise of systems that the high privileged account is authorised to access:")
+        check_reuse_pass(full_list,'summary')
+
+
         if args.admin_path:
             admin_summary_list = []
             for item in full_list:
@@ -788,7 +826,7 @@ if __name__ == "__main__":
                     admin_summary_list.append(check_admin(user,pwd))
 
             if len(admin_summary_list) > 0:
-                
+
                 print ("\nFinally, there were " + str(len(admin_summary_list)) + " administrative accounts (based upon 'Domain Admins', 'Enterprise Admins', etc.) that were able to be compromised. The account names and their respective passwords (masked) can be seen below:")
                 for admin_summary_pass in admin_summary_list:
                     print (admin_summary_pass)
@@ -823,7 +861,7 @@ if __name__ == "__main__":
                 if pwd == "":
                     pwd = "*******BLANK-PASS*******"
                 check_user_as_pass(user,pwd)
-                
+
             if args.admin_path is not None:
                 if args.output_report and admin_count == 0:
                     print ("\nThe following user accounts were identified as Domain Administrators (Domain Admins, Enterprise Admins, Administrators, etc) and were found to have weak passwords set: ")
@@ -840,6 +878,9 @@ if __name__ == "__main__":
                 shared_count += 1
                 check_shared_pass(full_list)
 
+            print ("\nThe following accounts were found to share the same password between similarly named accounts. These are believed to be service accounts or individual user accounts that are operated by the same user. Password re-use should be investigated as when a password is reused between a low privileged and a high privileged account it can lead to a compromise of systems that the high privileged account is authorised to access:")
+            check_reuse_pass(full_list,'summary')
+
 
             print ("\nThe following user accounts were found to have a password that was a variation of a day or date (e.g. Monday01 or September2016):")
             for item in full_list:
@@ -848,7 +889,7 @@ if __name__ == "__main__":
                 if pwd == "":
                     pwd = "*******BLANK-PASS*******"
                 check_date_day(user,pwd)
-                
+
             if args.output_report and common_count == 0:
                 print ("\nThe following user accounts were found to have a password that was a variation of the most common user passwords, which can include 'password', 'letmein', '123456', 'admin', 'iloveyou', 'friday', or 'qwerty':")
                 common_count += 1
@@ -858,12 +899,12 @@ if __name__ == "__main__":
                     if pwd == "":
                         pwd = "*******BLANK-PASS*******"
                     check_common_pass(user,pwd)
-                
+
             if args.output_report and keyboard_count == 0:
                 print ("\nThe following user accounts were identified as having passwords that utilise common keyboard patterns such as qwer, zxcvbn, qazwsx, etc.: ")
                 keyboard_count += 1
                 keyboard_patterns(full_list)
-                
+
             sys.exit() # Skip analysis functions below
 
         # Check for passwords that don't meet Min Length
@@ -975,13 +1016,13 @@ if __name__ == "__main__":
                 print ("\nThe following user accounts were found to have a passwords set that are re-used within other user accounts (with '*' representing a masked character). Usually, this is a coincidence with accounts using 'standard' weak password (such as 'Password1' or 'qwerty123', however where privileged/administrative accounts are used these should be reviewed further: ")
                 shared_count += 1
             check_shared_pass(full_list)
-            
+
         if args.keyboard_pattern:
             if args.output_report and keyboard_count == 0:
                 print ("\nThe following user accounts were identified as having passwords that utilise common keyboard patterns such as qwer, zxcvbn, qazwsx, etc.: ")
                 keyboard_count += 1
             keyboard_patterns(full_list)
-            
+
         if args.masks:
             if args.output_report is False:
                 output_pass("-" * 30,"-" * 30,"-" * 30)
