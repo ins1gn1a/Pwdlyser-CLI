db0726d1f3441902080092cbaab0857a824c5668
Adam <ins1gn1a@users.noreply.github.com>
2017-11-12 21:13:56 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo cp pwdlyser.py /usr/local/bin/pwdlyser
-sudo mkdir /etc/pwdlyser/
-sudo cp  *.conf /etc/pwdlyser/
+echo "[!] Sudo needed to copy to /etc/ and /usr/ directories:"
+echo "- Pwdlyser.py to /usr/local/bin/"
+echo "- Config files to /etc/pwdlyser/"
+sudo -H cp pwdlyser.py /usr/local/bin/pwdlyser
+sudo -H mkdir /etc/pwdlyser/
+sudo -H cp  *.conf /etc/pwdlyser/
